4574290f81b4bb924424a5b8762618ec9d041dd3
Abril Diaco <adiaco@udesa.edu.ar>
2025-04-03 11:40:02 -0300

    .

--- Ej4/main4.cpp
+++ Ej4/main4.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 using namespace std;
 
+//g++ -std=c++20 main4.cpp cuenta_banco.cpp cuenta_corriente.cpp caja_ahorro.cpp -o main4
+
 int main(){
     string nombre ="Abril diaco";
     CajaDeAhorro miahorro(nombre);
